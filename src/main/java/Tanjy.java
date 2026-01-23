d0327c7ef35e6df5765cae36fe401077916fa6f1
tanjy10 <harrontan2003@gmail.com>
2026-01-23 15:57:52 +0800

    Add echo

--- src/main/java/Tanjy.java
+++ src/main/java/Tanjy.java
@@ -1,10 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Tanjy {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        String border = "____________________________________________________________\n";
+
+        String intro = border
                 + "Hello! I'm Tanjy.\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n";
-        System.out.println(logo);
+                + border;
+        String outro = border
+                + "Bye. Hope to see you again soon!\n"
+                + border;
+
+        System.out.println(intro);
+        Scanner scanner = new Scanner(System.in);
+        String echo = scanner.next();
+
+        while (!Objects.equals(echo, "bye")) {
+            System.out.print(border + echo + "\n" + border);
+            echo = scanner.next();
+        }
+
+        System.out.print(outro);
     }
 }
