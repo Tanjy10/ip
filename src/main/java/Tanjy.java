3a665d422d795b40da90c2faf93b6eaa25ff710c
tanjy10 <harrontan2003@gmail.com>
2026-01-23 16:33:23 +0800

    Add Add, List

--- src/main/java/Tanjy.java
+++ src/main/java/Tanjy.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Tanjy {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
         String border = "____________________________________________________________\n";
 
         String intro = border
                 + "Hello! I'm Tanjy.\n"
                 + "What can I do for you?\n"
                 + border;
+
         String outro = border
                 + "Bye. Hope to see you again soon!\n"
                 + border;
@@ -18,8 +21,18 @@ public class Tanjy {
         String echo = scanner.next();
 
         while (!Objects.equals(echo, "bye")) {
-            System.out.print(border + echo + "\n" + border);
-            echo = scanner.next();
+            if (!Objects.equals(echo, "list")) {
+                System.out.print(border + "added: " + echo + "\n" + border);
+                list.add(echo);
+                echo = scanner.next();
+            }
+
+            if (Objects.equals(echo, "list")){
+                for (int i = 0; i < list.size(); i++){
+                    System.out.print((i + 1) + ". " + list.get(i) + "\n");
+                }
+                echo = scanner.next();
+            }
         }
 
         System.out.print(outro);
